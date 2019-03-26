8c38f5b0c1f55d1a9c1ac063f611568b5299f7c7
Michael Ulryck <michael.ulryck@gmail.com>
2019-03-26 18:12:45 +0800

    crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #show all tasks
+  get "tasks", to: "tasks#index", as: "tasks"
+
+
+
+  #Create a task
+  get "tasks/new", to: "tasks#new", as: "new"
+  post "tasks", to: "tasks#create", as: "create"
+
+  #Show specific task
+  get "tasks/:id", to: "tasks#show", as: "task"
+
+  #edit specific task
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit"
+  patch "tasks/:id", to: "tasks#update"
+
+  #delete speific task
+  delete "tasks/:id", to: "tasks#destroy"
+
 end
